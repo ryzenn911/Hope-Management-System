ab5a61b63ba9bbdf90e8bc3dc65cd683aed767fe
ryzenn911 <suzukijakata@gmail.com>
2026-06-01 23:10:28 +0700

    update01

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        // 2. បន្ថែមកូដ ២ បន្ទាត់នេះចូលក្នុង function boot()
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
