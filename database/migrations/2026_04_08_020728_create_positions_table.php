c9b99c4f0aa784c93a9de65e59a7b2bd8130ac97
ryzenn911 <suzukijakata@gmail.com>
2026-06-15 15:44:43 +0700

    update database001

--- database/migrations/2026_04_08_020728_create_positions_table.php
+++ database/migrations/2026_04_08_020728_create_positions_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
